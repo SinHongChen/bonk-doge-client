6e525969185911fce3144e0cd3e53912a42572a1
SinHongChen <a09342712201@gmail.com>
2022-01-05 13:27:46 +0800

    移除之前為了測試建立的 socket 頁面

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ export { default as Login } from "./Login";
 export { default as GamesLobby } from "./GamesLobby";
 export { default as Cards } from "./Cards";
 export { default as Account } from "./Account";
-export { default as Socket } from "./Socket";
 export { default as Game } from "./Game";
 export { default as Logout } from "./Logout";
 export { default as DeckEdit } from "./DeckEdit";

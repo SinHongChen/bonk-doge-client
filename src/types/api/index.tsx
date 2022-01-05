3248c3fcad632518866acf649ad46f48f7eb0209
SinHongChen <a09342712201@gmail.com>
2022-01-05 13:27:18 +0800

    新增遊戲 socket 的 type

--- src/types/api/index.tsx
+++ src/types/api/index.tsx
@@ -2,4 +2,4 @@ export type { default as UserLogin } from "./UserLogin";
 export type { default as UserInfo } from "./UserInfo";
 export type { default as CardInfo } from "./CardInfo";
 export type { default as DeckInfo } from "./DeckInfo";
-export type { Nature,Race,Attribute } from "./CardInfo";
+export type { Nature,Race,Attribute,RoleCardInfo,EffectCardInfo } from "./CardInfo";
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -3,3 +3,8 @@ export type { default as UserLogin } from "./api/UserLogin";
 export type { default as UserInfo } from "./api/UserInfo";
 export type { default as CardInfo } from "./api/CardInfo";
 export type { default as DeckInfo } from "./api/DeckInfo";
+export type { default as GameBoardData } from "./socket/GameBoardData";
+export type { Nature,Race,Attribute,RoleCardInfo,EffectCardInfo } from "./api/CardInfo";
+export type { default as SocketResponse } from "./socket/SocketResponse";
+export type { default as SocketRequest } from "./socket/SocketRequest";
+export type { default as GameBoard } from "./socket/GameBoard";
